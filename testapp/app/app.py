c69c989a61572af3d895b8f3608af9023a0d2125
gstvo2k15 <48087822+gstvo2k15@users.noreply.github.com>
2024-05-26 00:25:47 +0200

    Added scan to app.py with pylint-end2

--- testapp/app/app.py
+++ testapp/app/app.py
@@ -12,11 +12,13 @@ app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    'DATABASE_URL', 'postgresql://postgres:postgres@db:5432/postgres'
+    'DATABASE_URL',
+    'postgresql://postgres:postgres@db:5432/postgres'
 )
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
 
 # pylint: disable=too-few-public-methods
 class Visitor(db.Model):
@@ -31,16 +33,18 @@ class Visitor(db.Model):
     def __repr__(self):
         return f'<Visitor {self.ip}>'
 
+
 # Desactivar chequeo no-member de pylint
 # pylint: disable=no-member
 @app.before_first_request
-# pylint: disable=no-member
 def create_tables():
     """
     Crear las tablas de la base de datos antes de la primera solicitud.
     """
-    # pylint: disable=no-member
     db.create_all()
+# Volver a habilitar el chequeo no-member de pylint
+# pylint: enable=no-member
+
 
 @app.route('/')
 def index():
@@ -48,11 +52,15 @@ def index():
     Registra la IP del visitante y retorna el número de visitantes únicos.
     """
     try:
-        ip = request.headers.get('X-Forwarded-For', request.remote_addr).split(',')[0].strip()
+        ip = request.headers.get(
+            'X-Forwarded-For', request.remote_addr
+        ).split(',')[0].strip()
         new_visitor = Visitor(ip=ip)
         db.session.add(new_visitor)
         db.session.commit()
-        unique_visitors = db.session.query(db.func.count(db.distinct(Visitor.ip))).scalar()
+        unique_visitors = db.session.query(
+            db.func.count(db.distinct(Visitor.ip))
+        ).scalar()
         return jsonify(unique_visitors=unique_visitors)
     except ValueError as ve:
         return jsonify(error=str(ve)), 400
@@ -63,12 +71,14 @@ def index():
     except Exception as e:  # pylint: disable=broad-exception-caught
         return jsonify(error=str(e)), 500
 
+
 @app.route('/version')
 def version():
     """
     Retorna la versión de la aplicación.
     """
     return jsonify({"version": "1.0.0"})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
