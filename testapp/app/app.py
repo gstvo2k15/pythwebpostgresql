681920b544aae334e267e9dfc308816965a5e037
gstvo2k15 <48087822+gstvo2k15@users.noreply.github.com>
2024-05-26 00:04:24 +0200

    Added scan to app.py with autopep8 lint-2

--- testapp/app/app.py
+++ testapp/app/app.py
@@ -1,6 +1,6 @@
 """
-Este módulo proporciona una aplicación Flask simple que registra las visitas de los usuarios y
-expone métricas a través de Prometheus.
+App Flask simple para registrar visitas de usuarios y
+exponer las métricas a Prometheus.
 """
 
 import os
@@ -18,7 +18,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-
 class Visitor(db.Model):
     """
     Modelo de base de datos para registrar las visitas de los usuarios.
@@ -31,6 +30,7 @@ class Visitor(db.Model):
     def __repr__(self):
         return f'<Visitor {self.ip}>'
 
+    # pylint: disable=too-few-public-methods
 
 @app.before_first_request
 def create_tables():
@@ -38,25 +38,28 @@ def create_tables():
     Crear las tablas de la base de datos antes de la primera solicitud.
     """
     db.create_all()
-
+    # pylint: disable=no-member
 
 @app.route('/')
 def index():
     """
-    Maneja la solicitud a la ruta raíz. Registra la IP del visitante y retorna el número de visitantes únicos.
+    Registra la IP del visitante y retorna el número de visitantes únicos.
     """
     try:
-        ip = request.headers.get(
-            'X-Forwarded-For', request.remote_addr).split(',')[0].strip()
+        ip = request.headers.get('X-Forwarded-For', request.remote_addr).split(',')[0].strip()
         new_visitor = Visitor(ip=ip)
         db.session.add(new_visitor)
         db.session.commit()
-        unique_visitors = db.session.query(
-            db.func.count(db.distinct(Visitor.ip))).scalar()
+        unique_visitors = db.session.query(db.func.count(db.distinct(Visitor.ip))).scalar()
         return jsonify(unique_visitors=unique_visitors)
-    except Exception as e:
+    except ValueError as ve:
+        return jsonify(error=str(ve)), 400
+    except TypeError as te:
+        return jsonify(error=str(te)), 400
+    except db.exc.SQLAlchemyError as se:
+        return jsonify(error=str(se)), 500
+    except Exception as e:  # pylint: disable=broad-exception-caught
         return jsonify(error=str(e)), 500
-
 
 @app.route('/version')
 def version():
@@ -64,7 +67,6 @@ def version():
     Retorna la versión de la aplicación.
     """
     return jsonify({"version": "1.0.0"})
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
