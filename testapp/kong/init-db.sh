9d79d370826c4b10daa8ff3001338745f9b6aa06
Gustavo Olmo <gstvo2k15@gmail.com>
2024-07-17 21:42:20 +0200

    Added fix for kong and others checkcode containers-1b

---
+++ testapp/kong/init-db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER kong WITH PASSWORD 'kong';
+    CREATE DATABASE kong OWNER kong;
+    GRANT ALL PRIVILEGES ON DATABASE kong TO kong;
+EOSQL
+
